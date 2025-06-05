da2926304999ff2c84a257014b8bb3b425a614bf
Sabir Suleyman <sabir.suleyman.az@gmail.com>
2025-06-05 15:02:04 +0200

    projekt 2025

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # finalproject-dci
+# cd backend: npm install dotenv
+# cd frontend: npm install
+# cd frontend: npm install -g vite
+# cd frontend: npx vite
+# cd frontend: 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
+    open: true,
     proxy: {
       '/api': 'http://localhost:5000',
     },
